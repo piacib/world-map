6e348fe6c06240805d41a9a5466729b0997a4d60
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-06-06 10:23:26 -1000

    added folder structure

---
+++ src/hooks/usePrevious.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from "react";
+
+function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+export default usePrevious;
