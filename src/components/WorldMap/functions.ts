6e1900961f9e701db7f78bef13c2407022a9abeb
piacib <ben.piacitelli@gmail.com>
2022-07-30 16:46:10 -1000

    moved to seperate file from worldMap.tsx

---
+++ src/components/WorldMap/functions.ts
@@ -0,0 +1,26 @@
+export const getRandomKey = (obj: Object) => {
+  const keys = Object.keys(obj);
+  return keys[Math.floor(Math.random() * keys.length - 1)];
+};
+export const executeScroll = (element: HTMLElement | null) => {
+  if (element === null) {
+    return;
+  }
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
+};
+export const colorElement = (element: HTMLElement | null) => {
+  if (element === null) {
+    return;
+  }
+  element.classList.add("highlight");
+};
+export const removeColorFromElement = (element: HTMLElement | null) => {
+  if (element === null) {
+    return;
+  }
+  element.classList.remove("highlight");
+};
