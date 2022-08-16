b91ac2e1ae7d7a790c92c10011939a76cabdf349
piacib <ben.piacitelli@gmail.com>
2022-08-15 15:15:40 -1000

    added countinent option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ContinentOptions from "./components/ContinentOptions/ContinentOptions";
 import WorldMap from "./components/WorldMap/WorldMap";
-
+import { ContinentType } from "./components/ContinentOptions/ContinentOptions";
 function App() {
+  const [continent, setContinent] = useState<ContinentType | null>(null);
   return (
     <>
+      <ContinentOptions
+        setOption={(option: ContinentType) => setContinent(option)}
+      />
       <WorldMap />
     </>
   );
