30aa4f100323eef3756819715825f8c68e2c2399
piacib <ben.piacitelli@gmail.com>
2022-08-18 11:23:37 -1000

    added continent prop to worldmap

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import ContinentOptions from "./components/ContinentOptions/ContinentOptions";
 import WorldMap from "./components/WorldMap/WorldMap";
-import { ContinentType } from "./components/ContinentOptions/ContinentOptions";
+import { ContinentType } from "./countries";
 function App() {
   const [continent, setContinent] = useState<ContinentType | null>(null);
   return (
@@ -10,7 +10,7 @@ function App() {
       <ContinentOptions
         setOption={(option: ContinentType) => setContinent(option)}
       />
-      <WorldMap />
+      <WorldMap continent={continent} />
     </>
   );
 }
