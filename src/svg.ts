cce851f674ab8df2193318e8953aec8f7605dfc2
piacib <ben.piacitelli@gmail.com>
2022-08-18 13:31:36 -1000

    added types

--- src/svg.ts
+++ src/svg.ts
@@ -1275,519 +1275,516 @@ export const svgPaths = [
     id: "ZW",
   },
 ];
-const countryNames = [
-  "Andorra",
-  "United Arab Emirates",
-  "Afghanistan",
-  "Antigua and Barbuda",
-  "Anguilla",
-  "Albania",
-  "Armenia",
-  "Angola",
-  "Argentina",
-  "American Samoa",
-  "Austria",
-  "Australia",
-  "Aruba",
-  "Aland Islands",
-  "Azerbaijan",
-  "Bosnia and Herzegovina",
-  "Barbados",
-  "Bangladesh",
-  "Belgium",
-  "Burkina Faso",
-  "Bulgaria",
-  "Bahrain",
-  "Burundi",
-  "Benin",
-  "Saint Barthelemy",
-  "Brunei",
-  "Bolivia",
-  "Bermuda",
-  "Bonaire,  Saint Eustachius and Saba",
-  "Brazil",
-  "Bahamas",
-  "Bhutan",
-  "Bouvet Island",
-  "Botswana",
-  "Belarus",
-  "Belize",
-  "Canada",
-  "Cocos  (Keeling)  Islands",
-  "Democratic Republic of Congo",
-  "Central African Republic",
-  "Republic of Congo",
-  "Switzerland",
-  "Côte d'Ivoire",
-  "Cook Islands",
-  "Chile",
-  "Cameroon",
-  "China",
-  "Colombia",
-  "Costa Rica",
-  "Cuba",
-  "Cape Verde",
-  "Curaçao",
-  "Christmas Island",
-  "Cyprus",
-  "Czech Republic",
-  "Germany",
-  "Djibouti",
-  "Denmark",
-  "Dominica",
-  "Dominican Republic",
-  "Algeria",
-  "Ecuador",
-  "Egypt",
-  "Estonia",
-  "Western Sahara",
-  "Eritrea",
-  "Spain",
-  "Ethiopia",
-  "Finland",
-  "Fiji",
-  "Falkland Islands",
-  "Federated States of Micronesia",
-  "Faroe Islands",
-  "France",
-  "Gabon",
-  "United Kingdom",
-  "Georgia",
-  "Grenada",
-  "French Guiana",
-  "Guernsey",
-  "Ghana",
-  "Gibraltar",
-  "Greenland",
-  "Gambia",
-  "Guinea",
-  "Glorioso Islands",
-  "Guadeloupe",
-  "Equatorial Guinea",
-  "Greece",
-  "South Georgia and the South Sandwich Islands",
-  "Guatemala",
-  "Guam",
-  "Guinea-Bissau",
-  "Guyana",
-  "Hong Kong",
-  "Heard Island and McDonald Islands",
-  "Honduras",
-  "Croatia",
-  "Haiti",
-  "Hungary",
-  "Indonesia",
-  "Ireland",
-  "Israel",
-  "Isle of Man",
-  "India",
-  "British Indian Ocean Territory",
-  "Iraq",
-  "Iran",
-  "Iceland",
-  "Italy",
-  "Jersey",
-  "Jamaica",
-  "Jordan",
-  "Japan",
-  "Juan De Nova Island",
-  "Kenya",
-  "Kyrgyzstan",
-  "Cambodia",
-  "Kiribati",
-  "Comoros",
-  "Saint Kitts and Nevis",
-  "North Korea",
-  "South Korea",
-  "Kosovo",
-  "Kuwait",
-  "Cayman Islands",
-  "Kazakhstan",
-  "Laos",
-  "Lebanon",
-  "Saint Lucia",
-  "Liechtenstein",
-  "Sri Lanka",
-  "Liberia",
-  "Lesotho",
-  "Lithuania",
-  "Luxembourg",
-  "Latvia",
-  "Libya",
-  "Morocco",
-  "Monaco",
-  "Moldova",
-  "Madagascar",
-  "Montenegro",
-  "Saint Martin",
-  "Marshall Islands",
-  "North Macedonia",
-  "Mali",
-  "Macau",
-  "Myanmar",
-  "Mongolia",
-  "Northern Mariana Islands",
-  "Martinique",
-  "Mauritania",
-  "Montserrat",
-  "Malta",
-  "Mauritius",
-  "Maldives",
-  "Malawi",
-  "Mexico",
-  "Malaysia",
-  "Mozambique",
-  "Namibia",
-  "New Caledonia",
-  "Niger",
-  "Norfolk Island",
-  "Nigeria",
-  "Nicaragua",
-  "Netherlands",
-  "Norway",
-  "Nepal",
-  "Nauru",
-  "Niue",
-  "New Zealand",
-  "Oman",
-  "Panama",
-  "Peru",
-  "Papua New Guinea",
-  "Philippines",
-  "Pakistan",
-  "Poland",
-  "Saint Pierre and Miquelon",
-  "Pitcairn Islands",
-  "Puerto Rico",
-  "Palestine",
-  "Portugal",
-  "Palau",
-  "Paraguay",
-  "Qatar",
-  "Reunion",
-  "Romania",
-  "Serbia",
-  "Russia",
-  "Rwanda",
-  "Saudi Arabia",
-  "Solomon Islands",
-  "Seychelles",
-  "Sudan",
-  "Sweden",
-  "Singapore",
-  "Saint Helena",
-  "Slovenia",
-  "Svalbard and Jan Mayen",
-  "Slovakia",
-  "Sierra Leone",
-  "San Marino",
-  "Senegal",
-  "Somalia",
-  "Suriname",
-  "South Sudan",
-  "Sao Tome and Principe",
-  "El Salvador",
-  "Saint Martin",
-  "Syria",
-  "Swaziland",
-  "Turks and Caicos Islands",
-  "Chad",
-  "French Southern and Antarctic Lands",
-  "Togo",
-  "Thailand",
-  "Tajikistan",
-  "Tokelau",
-  "Timor-Leste",
-  "Turkmenistan",
-  "Tunisia",
-  "Tonga",
-  "Turkey",
-  "Trinidad and Tobago",
-  "Tuvalu",
-  "Taiwan",
-  "Tanzania",
-  "Ukraine",
-  "Uganda",
-  "Jarvis Island",
-  "Baker Island",
-  "Howland Island",
-  "Johnston Atoll",
-  "Midway Islands",
-  "Wake Island",
-  "United States",
-  "Uruguay",
-  "Uzbekistan",
-  "Vatican City",
-  "Saint Vincent and the Grenadines",
-  "Venezuela",
-  "British Virgin Islands",
-  "US Virgin Islands",
-  "Vietnam",
-  "Vanuatu",
-  "Wallis and Futuna",
-  "Samoa",
-  "Yemen",
-  "Mayotte",
-  "South Africa",
-  "Zambia",
-  "Zimbabwe",
-] as const;
-export type CountryNameType = typeof countryNames[number];
-const countryIds = [
-  "AD",
-  "AE",
-  "AF",
-  "AG",
-  "AI",
-  "AL",
-  "AM",
-  "AO",
-  "AR",
-  "AS",
-  "AT",
-  "AU",
-  "AW",
-  "AX",
-  "AZ",
-  "BA",
-  "BB",
-  "BD",
-  "BE",
-  "BF",
-  "BG",
-  "BH",
-  "BI",
-  "BJ",
-  "BL",
-  "BN",
-  "BO",
-  "BM",
-  "BQ",
-  "BR",
-  "BS",
-  "BT",
-  "BV",
-  "BW",
-  "BY",
-  "BZ",
-  "CA",
-  "CC",
-  "CD",
-  "CF",
-  "CG",
-  "CH",
-  "CI",
-  "CK",
-  "CL",
-  "CM",
-  "CN",
-  "CO",
-  "CR",
-  "CU",
-  "CV",
-  "CW",
-  "CX",
-  "CY",
-  "CZ",
-  "DE",
-  "DJ",
-  "DK",
-  "DM",
-  "DO",
-  "DZ",
-  "EC",
-  "EG",
-  "EE",
-  "EH",
-  "ER",
-  "ES",
-  "ET",
-  "FI",
-  "FJ",
-  "FK",
-  "FM",
-  "FO",
-  "FR",
-  "GA",
-  "GB",
-  "GE",
-  "GD",
-  "GF",
-  "GG",
-  "GH",
-  "GI",
-  "GL",
-  "GM",
-  "GN",
-  "GO",
-  "GP",
-  "GQ",
-  "GR",
-  "GS",
-  "GT",
-  "GU",
-  "GW",
-  "GY",
-  "HK",
-  "HM",
-  "HN",
-  "HR",
-  "HT",
-  "HU",
-  "ID",
-  "IE",
-  "IL",
-  "IM",
-  "IN",
-  "IO",
-  "IQ",
-  "IR",
-  "IS",
-  "IT",
-  "JE",
-  "JM",
-  "JO",
-  "JP",
-  "JU",
-  "KE",
-  "KG",
-  "KH",
-  "KI",
-  "KM",
-  "KN",
-  "KP",
-  "KR",
-  "XK",
-  "KW",
-  "KY",
-  "KZ",
-  "LA",
-  "LB",
-  "LC",
-  "LI",
-  "LK",
-  "LR",
-  "LS",
-  "LT",
-  "LU",
-  "LV",
-  "LY",
-  "MA",
-  "MC",
-  "MD",
-  "MG",
-  "ME",
-  "MF",
-  "MH",
-  "MK",
-  "ML",
-  "MO",
-  "MM",
-  "MN",
-  "MP",
-  "MQ",
-  "MR",
-  "MS",
-  "MT",
-  "MU",
-  "MV",
-  "MW",
-  "MX",
-  "MY",
-  "MZ",
-  "NA",
-  "NC",
-  "NE",
-  "NF",
-  "NG",
-  "NI",
-  "NL",
-  "NO",
-  "NP",
-  "NR",
-  "NU",
-  "NZ",
-  "OM",
-  "PA",
-  "PE",
-  "PG",
-  "PH",
-  "PK",
-  "PL",
-  "PM",
-  "PN",
-  "PR",
-  "PS",
-  "PT",
-  "PW",
-  "PY",
-  "QA",
-  "RE",
-  "RO",
-  "RS",
-  "RU",
-  "RW",
-  "SA",
-  "SB",
-  "SC",
-  "SD",
-  "SE",
-  "SG",
-  "SH",
-  "SI",
-  "SJ",
-  "SK",
-  "SL",
-  "SM",
-  "SN",
-  "SO",
-  "SR",
-  "SS",
-  "ST",
-  "SV",
-  "SX",
-  "SY",
-  "SZ",
-  "TC",
-  "TD",
-  "TF",
-  "TG",
-  "TH",
-  "TJ",
-  "TK",
-  "TL",
-  "TM",
-  "TN",
-  "TO",
-  "TR",
-  "TT",
-  "TV",
-  "TW",
-  "TZ",
-  "UA",
-  "UG",
-  "UM-DQ",
-  "UM-FQ",
-  "UM-HQ",
-  "UM-JQ",
-  "UM-MQ",
-  "UM-WQ",
-  "US",
-  "UY",
-  "UZ",
-  "VA",
-  "VC",
-  "VE",
-  "VG",
-  "VI",
-  "VN",
-  "VU",
-  "WF",
-  "WS",
-  "YE",
-  "YT",
-  "ZA",
-  "ZM",
-  "ZW",
-] as const;
-export type CountryIdType = typeof countryIds[number];
+
+export type CountryCodeType =
+  | "AD"
+  | "AE"
+  | "AF"
+  | "AG"
+  | "AI"
+  | "AL"
+  | "AM"
+  | "AO"
+  | "AR"
+  | "AS"
+  | "AT"
+  | "AU"
+  | "AW"
+  | "AX"
+  | "AZ"
+  | "BA"
+  | "BB"
+  | "BD"
+  | "BE"
+  | "BF"
+  | "BG"
+  | "BH"
+  | "BI"
+  | "BJ"
+  | "BL"
+  | "BN"
+  | "BO"
+  | "BM"
+  | "BQ"
+  | "BR"
+  | "BS"
+  | "BT"
+  | "BV"
+  | "BW"
+  | "BY"
+  | "BZ"
+  | "CA"
+  | "CC"
+  | "CD"
+  | "CF"
+  | "CG"
+  | "CH"
+  | "CI"
+  | "CK"
+  | "CL"
+  | "CM"
+  | "CN"
+  | "CO"
+  | "CR"
+  | "CU"
+  | "CV"
+  | "CW"
+  | "CX"
+  | "CY"
+  | "CZ"
+  | "DE"
+  | "DJ"
+  | "DK"
+  | "DM"
+  | "DO"
+  | "DZ"
+  | "EC"
+  | "EG"
+  | "EE"
+  | "EH"
+  | "ER"
+  | "ES"
+  | "ET"
+  | "FI"
+  | "FJ"
+  | "FK"
+  | "FM"
+  | "FO"
+  | "FR"
+  | "GA"
+  | "GB"
+  | "GE"
+  | "GD"
+  | "GF"
+  | "GG"
+  | "GH"
+  | "GI"
+  | "GL"
+  | "GM"
+  | "GN"
+  | "GO"
+  | "GP"
+  | "GQ"
+  | "GR"
+  | "GS"
+  | "GT"
+  | "GU"
+  | "GW"
+  | "GY"
+  | "HK"
+  | "HM"
+  | "HN"
+  | "HR"
+  | "HT"
+  | "HU"
+  | "ID"
+  | "IE"
+  | "IL"
+  | "IM"
+  | "IN"
+  | "IO"
+  | "IQ"
+  | "IR"
+  | "IS"
+  | "IT"
+  | "JE"
+  | "JM"
+  | "JO"
+  | "JP"
+  | "JU"
+  | "KE"
+  | "KG"
+  | "KH"
+  | "KI"
+  | "KM"
+  | "KN"
+  | "KP"
+  | "KR"
+  | "XK"
+  | "KW"
+  | "KY"
+  | "KZ"
+  | "LA"
+  | "LB"
+  | "LC"
+  | "LI"
+  | "LK"
+  | "LR"
+  | "LS"
+  | "LT"
+  | "LU"
+  | "LV"
+  | "LY"
+  | "MA"
+  | "MC"
+  | "MD"
+  | "MG"
+  | "ME"
+  | "MF"
+  | "MH"
+  | "MK"
+  | "ML"
+  | "MO"
+  | "MM"
+  | "MN"
+  | "MP"
+  | "MQ"
+  | "MR"
+  | "MS"
+  | "MT"
+  | "MU"
+  | "MV"
+  | "MW"
+  | "MX"
+  | "MY"
+  | "MZ"
+  | "NA"
+  | "NC"
+  | "NE"
+  | "NF"
+  | "NG"
+  | "NI"
+  | "NL"
+  | "NO"
+  | "NP"
+  | "NR"
+  | "NU"
+  | "NZ"
+  | "OM"
+  | "PA"
+  | "PE"
+  | "PG"
+  | "PH"
+  | "PK"
+  | "PL"
+  | "PM"
+  | "PN"
+  | "PR"
+  | "PS"
+  | "PT"
+  | "PW"
+  | "PY"
+  | "QA"
+  | "RE"
+  | "RO"
+  | "RS"
+  | "RU"
+  | "RW"
+  | "SA"
+  | "SB"
+  | "SC"
+  | "SD"
+  | "SE"
+  | "SG"
+  | "SH"
+  | "SI"
+  | "SJ"
+  | "SK"
+  | "SL"
+  | "SM"
+  | "SN"
+  | "SO"
+  | "SR"
+  | "SS"
+  | "ST"
+  | "SV"
+  | "SX"
+  | "SY"
+  | "SZ"
+  | "TC"
+  | "TD"
+  | "TF"
+  | "TG"
+  | "TH"
+  | "TJ"
+  | "TK"
+  | "TL"
+  | "TM"
+  | "TN"
+  | "TO"
+  | "TR"
+  | "TT"
+  | "TV"
+  | "TW"
+  | "TZ"
+  | "UA"
+  | "UG"
+  | "UM-DQ"
+  | "UM-FQ"
+  | "UM-HQ"
+  | "UM-JQ"
+  | "UM-MQ"
+  | "UM-WQ"
+  | "US"
+  | "UY"
+  | "UZ"
+  | "VA"
+  | "VC"
+  | "VE"
+  | "VG"
+  | "VI"
+  | "VN"
+  | "VU"
+  | "WF"
+  | "WS"
+  | "YE"
+  | "YT"
+  | "ZA"
+  | "ZM"
+  | "ZW";
+export type CountryNameType =
+  | "Andorra"
+  | "United Arab Emirates"
+  | "Afghanistan"
+  | "Antigua and Barbuda"
+  | "Anguilla"
+  | "Albania"
+  | "Armenia"
+  | "Angola"
+  | "Argentina"
+  | "American Samoa"
+  | "Austria"
+  | "Australia"
+  | "Aruba"
+  | "Aland Islands"
+  | "Azerbaijan"
+  | "Bosnia and Herzegovina"
+  | "Barbados"
+  | "Bangladesh"
+  | "Belgium"
+  | "Burkina Faso"
+  | "Bulgaria"
+  | "Bahrain"
+  | "Burundi"
+  | "Benin"
+  | "Saint Barthelemy"
+  | "Brunei"
+  | "Bolivia"
+  | "Bermuda"
+  | "Bonaire|  Saint Eustachius and Saba"
+  | "Brazil"
+  | "Bahamas"
+  | "Bhutan"
+  | "Bouvet Island"
+  | "Botswana"
+  | "Belarus"
+  | "Belize"
+  | "Canada"
+  | "Cocos  (Keeling)  Islands"
+  | "Democratic Republic of Congo"
+  | "Central African Republic"
+  | "Republic of Congo"
+  | "Switzerland"
+  | "Côte d'Ivoire"
+  | "Cook Islands"
+  | "Chile"
+  | "Cameroon"
+  | "China"
+  | "Colombia"
+  | "Costa Rica"
+  | "Cuba"
+  | "Cape Verde"
+  | "Curaçao"
+  | "Christmas Island"
+  | "Cyprus"
+  | "Czech Republic"
+  | "Germany"
+  | "Djibouti"
+  | "Denmark"
+  | "Dominica"
+  | "Dominican Republic"
+  | "Algeria"
+  | "Ecuador"
+  | "Egypt"
+  | "Estonia"
+  | "Western Sahara"
+  | "Eritrea"
+  | "Spain"
+  | "Ethiopia"
+  | "Finland"
+  | "Fiji"
+  | "Falkland Islands"
+  | "Federated States of Micronesia"
+  | "Faroe Islands"
+  | "France"
+  | "Gabon"
+  | "United Kingdom"
+  | "Georgia"
+  | "Grenada"
+  | "French Guiana"
+  | "Guernsey"
+  | "Ghana"
+  | "Gibraltar"
+  | "Greenland"
+  | "Gambia"
+  | "Guinea"
+  | "Glorioso Islands"
+  | "Guadeloupe"
+  | "Equatorial Guinea"
+  | "Greece"
+  | "South Georgia and the South Sandwich Islands"
+  | "Guatemala"
+  | "Guam"
+  | "Guinea-Bissau"
+  | "Guyana"
+  | "Hong Kong"
+  | "Heard Island and McDonald Islands"
+  | "Honduras"
+  | "Croatia"
+  | "Haiti"
+  | "Hungary"
+  | "Indonesia"
+  | "Ireland"
+  | "Israel"
+  | "Isle of Man"
+  | "India"
+  | "British Indian Ocean Territory"
+  | "Iraq"
+  | "Iran"
+  | "Iceland"
+  | "Italy"
+  | "Jersey"
+  | "Jamaica"
+  | "Jordan"
+  | "Japan"
+  | "Juan De Nova Island"
+  | "Kenya"
+  | "Kyrgyzstan"
+  | "Cambodia"
+  | "Kiribati"
+  | "Comoros"
+  | "Saint Kitts and Nevis"
+  | "North Korea"
+  | "South Korea"
+  | "Kosovo"
+  | "Kuwait"
+  | "Cayman Islands"
+  | "Kazakhstan"
+  | "Laos"
+  | "Lebanon"
+  | "Saint Lucia"
+  | "Liechtenstein"
+  | "Sri Lanka"
+  | "Liberia"
+  | "Lesotho"
+  | "Lithuania"
+  | "Luxembourg"
+  | "Latvia"
+  | "Libya"
+  | "Morocco"
+  | "Monaco"
+  | "Moldova"
+  | "Madagascar"
+  | "Montenegro"
+  | "Saint Martin"
+  | "Marshall Islands"
+  | "North Macedonia"
+  | "Mali"
+  | "Macau"
+  | "Myanmar"
+  | "Mongolia"
+  | "Northern Mariana Islands"
+  | "Martinique"
+  | "Mauritania"
+  | "Montserrat"
+  | "Malta"
+  | "Mauritius"
+  | "Maldives"
+  | "Malawi"
+  | "Mexico"
+  | "Malaysia"
+  | "Mozambique"
+  | "Namibia"
+  | "New Caledonia"
+  | "Niger"
+  | "Norfolk Island"
+  | "Nigeria"
+  | "Nicaragua"
+  | "Netherlands"
+  | "Norway"
+  | "Nepal"
+  | "Nauru"
+  | "Niue"
+  | "New Zealand"
+  | "Oman"
+  | "Panama"
+  | "Peru"
+  | "Papua New Guinea"
+  | "Philippines"
+  | "Pakistan"
+  | "Poland"
+  | "Saint Pierre and Miquelon"
+  | "Pitcairn Islands"
+  | "Puerto Rico"
+  | "Palestine"
+  | "Portugal"
+  | "Palau"
+  | "Paraguay"
+  | "Qatar"
+  | "Reunion"
+  | "Romania"
+  | "Serbia"
+  | "Russia"
+  | "Rwanda"
+  | "Saudi Arabia"
+  | "Solomon Islands"
+  | "Seychelles"
+  | "Sudan"
+  | "Sweden"
+  | "Singapore"
+  | "Saint Helena"
+  | "Slovenia"
+  | "Svalbard and Jan Mayen"
+  | "Slovakia"
+  | "Sierra Leone"
+  | "San Marino"
+  | "Senegal"
+  | "Somalia"
+  | "Suriname"
+  | "South Sudan"
+  | "Sao Tome and Principe"
+  | "El Salvador"
+  | "Saint Martin"
+  | "Syria"
+  | "Swaziland"
+  | "Turks and Caicos Islands"
+  | "Chad"
+  | "French Southern and Antarctic Lands"
+  | "Togo"
+  | "Thailand"
+  | "Tajikistan"
+  | "Tokelau"
+  | "Timor-Leste"
+  | "Turkmenistan"
+  | "Tunisia"
+  | "Tonga"
+  | "Turkey"
+  | "Trinidad and Tobago"
+  | "Tuvalu"
+  | "Taiwan"
+  | "Tanzania"
+  | "Ukraine"
+  | "Uganda"
+  | "Jarvis Island"
+  | "Baker Island"
+  | "Howland Island"
+  | "Johnston Atoll"
+  | "Midway Islands"
+  | "Wake Island"
+  | "United States"
+  | "Uruguay"
+  | "Uzbekistan"
+  | "Vatican City"
+  | "Saint Vincent and the Grenadines"
+  | "Venezuela"
+  | "British Virgin Islands"
+  | "US Virgin Islands"
+  | "Vietnam"
+  | "Vanuatu"
+  | "Wallis and Futuna"
+  | "Samoa"
+  | "Yemen"
+  | "Mayotte"
+  | "South Africa"
+  | "Zambia"
+  | "Zimbabwe";
