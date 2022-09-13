754630250d8f187b739876f4eb7fdb45a88d9b71
piacib <ben.piacitelli@gmail.com>
2022-09-13 11:01:45 -1000

    added app class and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from "react";
-import "./App.css";
-import ContinentOptions from "./components/ContinentOptions/ContinentOptions";
-import WorldMap from "./components/WorldMap/WorldMap";
-import { ContinentType } from "./countries";
-import { useLocalStorage } from "./hooks/useLocalStorage";
+import React from 'react';
+import './App.css';
+import ContinentOptions from './components/ContinentOptions/ContinentOptions';
+import WorldMap from './components/WorldMap/WorldMap';
+import { ContinentType } from './countries';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 function App() {
-  const [continent, setContinent] = useLocalStorage<ContinentType | null>(
-    "continent",
-    null
-  );
+  const [continent, setContinent] = useLocalStorage<ContinentType | null>('continent', null);
 
   return (
-    <>
-      <ContinentOptions
-        setOption={(option: ContinentType) => setContinent(option)}
-      />
+    <div className="app">
+      <ContinentOptions setOption={(option: ContinentType) => setContinent(option)} />
       <WorldMap continent={continent} />
-    </>
+    </div>
   );
 }
 
