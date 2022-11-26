cd480a94c93dbb6494870b318f8c02e788053d5b
piacib <ben.piacitelli@gmail.com>
2022-11-26 10:45:36 -1000

    updated imports

--- src/components/SvgDragAndZoom/hooks/useSvgCircle/index.ts
+++ src/components/SvgDragAndZoom/hooks/useSvgCircle/index.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { CircleCountryType } from "./types";
 import { SelectedCountryType } from "../../../WorldMap/types";
 const noCircle = {
