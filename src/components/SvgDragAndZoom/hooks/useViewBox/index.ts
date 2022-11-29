1e145812a5581aaaa5fcd9d3d56893b9cbc7998a
piacib <ben.piacitelli@gmail.com>
2022-11-29 10:31:05 -1000

    added size parameter to allow changing svg size

--- src/components/SvgDragAndZoom/hooks/useViewBox/index.ts
+++ src/components/SvgDragAndZoom/hooks/useViewBox/index.ts
@@ -7,45 +7,54 @@ interface Props {
   minScale?: number;
   scrollAdjustment?: number;
 }
+type Size = {
+  w: number;
+  h: number;
+};
 const useViewBox = ({
   svgWidth,
   svgHeight,
   maxScale,
   minScale,
   scrollAdjustment = 0.05,
-}: Props): [
-  [ViewBox, React.Dispatch<React.SetStateAction<ViewBox>>],
-  {
+}: Props): {
+  viewBox: ViewBox;
+  setViewBox: React.Dispatch<React.SetStateAction<ViewBox>>;
+  onMouse: {
     Wheel: (e: React.WheelEvent) => void;
     Down: (e: React.MouseEvent) => void;
     Move: (e: React.MouseEvent) => void;
     Up: (e: React.MouseEvent) => void;
     Leave: (e: React.MouseEvent) => void;
-  },
-  string,
-  number,
-] => {
+  };
+  viewBoxString: string;
+  scale: number;
+  size: Size;
+  setSize: React.Dispatch<React.SetStateAction<Size>>;
+} => {
+  const [size, setSize] = useState<Size>({ w: svgWidth, h: svgHeight });
   const [viewBox, setViewBox] = useState<ViewBox>({
     x: 0,
     y: 0,
-    w: svgWidth,
-    h: svgHeight,
+    w: size.w,
+    h: size.h,
   });
+
   const [isPanning, setIsPanning] = useState<Boolean>(false);
   const [startPoint, setStartPoint] = useState<StartPoint>({
     x: 0,
     y: 0,
   });
   const { x, y, w, h } = viewBox;
-  const scale = svgWidth / w; //sets zoom multiplier
+  const scale = size.w / w; //sets zoom multiplier
   const onMouseWheel = (e: React.WheelEvent) => {
     const mx = e.nativeEvent.offsetX; // mouse x offset from svgContainer left
     const my = e.nativeEvent.offsetY; // mouse Y offset from svgContainer top
     //  e.deltaY positive for scroll up negative for scroll down
     const dw = -w * Math.sign(e.deltaY) * scrollAdjustment; // enlarge if scroll up minimmize if scroll down
     const dh = -h * Math.sign(e.deltaY) * scrollAdjustment; // enlarge if scroll up minimmize if scroll down
-    const dx = (dw * mx) / svgWidth; // the adjustment in x to be displayed per scroll
-    const dy = (dh * my) / svgHeight; // the adjustment in y to be displayed per scroll
+    const dx = (dw * mx) / size.w; // the adjustment in x to be displayed per scroll
+    const dy = (dh * my) / size.h; // the adjustment in y to be displayed per scroll
     console.log(maxScale, scale, minScale);
     if (maxScale && maxScale >= scale && dw < 0) {
       return;
@@ -107,6 +116,6 @@ const useViewBox = ({
   };
   // pass onMouse to svg container
   // set svg viewbox={viewBoxString}
-  return [[viewBox, setViewBox], onMouse, viewBoxString, scale];
+  return { viewBox, setViewBox, onMouse, viewBoxString, scale, size, setSize };
 };
 export default useViewBox;
