59de68cd626a829caa6a44709a8e3273c25d6d1c
piacib <ben.piacitelli@gmail.com>
2022-12-29 12:51:16 -1000

    added Worldle

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ContinentOptions from "./components/ContinentOptions/ContinentOptions";
 import WorldMap from "./components/WorldMap/WorldMap";
 import { ContinentType } from "./countries";
 import { useLocalStorage } from "./hooks/useLocalStorage";
-
+import Worldle from "./components/Wordle";
 function App() {
   const [continent, setContinent] = useLocalStorage<ContinentType | null>("continent", null);
   console.log(continent);
@@ -12,6 +12,7 @@ function App() {
     <div className="app">
       <ContinentOptions setOption={(option: ContinentType) => setContinent(option)} />
       <WorldMap continent={continent} />
+      <Worldle />
     </div>
   );
 }
