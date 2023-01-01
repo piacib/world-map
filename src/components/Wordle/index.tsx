5306d0f01c709ba01e6ce92bd81a4ff9719d6543
piacib <ben.piacitelli@gmail.com>
2023-01-01 11:44:56 -1000

    added random country generator

--- src/components/Wordle/index.tsx
+++ src/components/Wordle/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { svgPaths } from "../../svg";
+const getIndex = () => {
+  const svgPathlength = 255;
+  const date = new Date();
+  const dateNum = date.getDay() * date.getMonth() * date.getFullYear();
+  return dateNum % svgPathlength;
+};
 const Worldle = () => {
-  return <div>Worldle</div>;
+  const country = svgPaths[getIndex()];
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={"0 0 900 900"}
+        height={900}
+        width={900}
+        id="map"
+        className={`map`}
+      >
+        <path d={country.d} id={country.id} key={country.id} />
+      </svg>
+    </div>
+  );
 };
 
 export default Worldle;
